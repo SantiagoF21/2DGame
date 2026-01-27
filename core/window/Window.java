76be1458637664132737f8319fcae70ca9e51d0f
SantiagoF21 <santiagofdz18@gmail.com>
2026-01-27 13:38:58 -0500

    Changed the edits again to reflect getYCoord() and getXCoord().

--- core/window/Window.java
+++ core/window/Window.java
@@ -46,11 +46,11 @@ public class Window {
         return height;
     }
 
-    public int getxCoord() {
+    public int getXCoord() {
         return xCoord;
     }
 
-    public int getyCoord() {
+    public int getYCoord() {
         return yCoord;
     }
 
