a0e17cefd2b42cdd53d9c9d543f10ff922023b52
SantiagoF21 <santiagofdz18@gmail.com>
2026-01-27 12:36:03 -0500

    Created a testWindow

--- launcher/Main.java
+++ launcher/Main.java
@@ -1,9 +1,14 @@
 package launcher;
 
+import core.window.Window;
+import core.window.WindowBuilder;
+import core.window.WindowRenderer;
+
 public class Main {
 
     public static void main(String[] args) {
-        
+        Window testWindow = new WindowBuilder().centerOnScreen().build();
+        WindowRenderer.render(testWindow);
     }
 
 }
