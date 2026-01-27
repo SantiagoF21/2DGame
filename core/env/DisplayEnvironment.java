80b7c0cd8e31304fd5c33021d7b3a549cd97ed3b
SantiagoF21 <santiagofdz18@gmail.com>
2026-01-27 11:22:56 -0500

    Added ScreenSize, ScreenWidth, and ScreenHeight as public static fields.

--- core/env/DisplayEnvironment.java
+++ core/env/DisplayEnvironment.java
@@ -1,8 +1,15 @@
 package core.env;
 
+import java.awt.Dimension;
+import java.awt.Toolkit;
 
 public class DisplayEnvironment {
 
+    private DisplayEnvironment() {}
     
+    public static final Dimension SCREEN_SIZE = Toolkit.getDefaultToolkit().getScreenSize();
+    
+    public static final int SCREEN_WIDTH = (int)SCREEN_SIZE.getWidth();
+    public static final int SCREEN_HEIGHT = (int)SCREEN_SIZE.getHeight();
 
 }
