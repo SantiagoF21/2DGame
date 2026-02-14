f92dd5383d01ade38c584a6de370465f3be9deab
SantiagoF21 <santiagofdz18@gmail.com>
2026-02-13 20:01:47 -0500

    Added many new fields such as isDirty, snapshot, isMinimized, and hasFocus.

--- core/window/Window.java
+++ core/window/Window.java
@@ -7,14 +7,14 @@ public class Window {
 
     /* CORE FIELDS */
 
-    /* Represents when a Window should redrawn on the EDT (Event Dispatch Thread) */
+    /* Flag that represents when a Window needs be rendered on the EDT (Event Dispatch Thread) */
     private boolean isDirty;
 
-    private WindowState liveWindow;
-    private WindowState renderWindow;
+    /* Implements Memento Design Pattern so that snapshots can be renderered on the EDT; Clear separation of states; Design -> Window.java (Originator), WindowMemento.java (Memento) N/A (Caretaker) */
+    private WindowMemento snapshot;
 
-    /* APPLICATION FIELDS */
-    
+    /* APP FIELDS */
+
     private int width;
     private int height;
 
@@ -24,93 +24,141 @@ public class Window {
     private Color backgroundColor;
 
     private boolean isVisible;
+    private boolean isMinimized;
+
     private boolean isFocusable;
+    private boolean hasFocus;
 
     private final String title;
     private final String appImageFilePath;
-    private final LayoutManager layout;
 
+    private final LayoutManager layout;
     private final boolean isManuallyResizable;
+
     private final boolean isDoubleBuffered;
+    
     private final boolean isOpaque;
 
-    protected Window(int width, int height, int xCoord, int yCoord, Color backgroundColor, boolean isVisible, boolean isFocusable, String title, String appImageFilePath, LayoutManager layout, boolean isManuallyResizable, boolean isDoubleBuffered, boolean isOpaque) {
+    /* Implements Builder Design Pattern so that devs can make Window instances without having to worry about argument order; Design -> Window.java (Product), WindowBuilder.java (Builder/ConcreteBuilder/Director), Custom Application (Client) */
+    protected Window(int width, int height, int xCoord, int yCoord, Color backgroundColor, boolean isVisible, boolean isMinimized, boolean isFocusable, boolean hasFocus, String title, String appImageFilePath, LayoutManager layout, boolean isManuallyResizable, boolean isDoubleBuffered, boolean isOpaque) {
         this.width = width;
         this.height = height;
         this.xCoord = xCoord;
         this.yCoord = yCoord;
         this.backgroundColor = backgroundColor;
         this.isVisible = isVisible;
+        this.isMinimized = isMinimized;
         this.isFocusable = isFocusable;
+        this.hasFocus = hasFocus;
         this.title = title;
         this.appImageFilePath = appImageFilePath;
         this.layout = layout;
         this.isManuallyResizable = isManuallyResizable;
         this.isDoubleBuffered = isDoubleBuffered;
         this.isOpaque = isOpaque;
-
-        /* Window when instanciated will be treated as dirty so that it can render for the first time.*/
-        this.isDirty = true;
-        /*  */
-        this.liveWindow = new WindowState();
-        this.renderWindow = new WindowState();
-        liveWindow.copyFrom(this);
-        renderWindow.copyFrom(liveWindow);
+        markDirty();
+        this.snapshot = new WindowMemento(this);
     }
 
-    protected synchronized void setWidth(int width) {
+    /* CORE METHODS */
+
+    /* Should only be called when Window is instantiated or mutable field is changed */
+    protected synchronized void markDirty() {
+        this.isDirty = true;
+    }
+
+    /* Should only be called when Window is instantiated or mutable field is changed */
+    protected synchronized void markClean() {
+        this.isDirty = false;
+    }
+
+    /* If window is considered dirty, a snapshot of our window's mutable fields will be taken at that instance. */
+    protected synchronized void updateSnapshot() {
+        if (isDirty) {
+            markClean();
+            snapshot.updateFrom(this);
+        }
+    }
+
+    /* Should only be called when Window is getting rendered */
+    protected synchronized boolean isDirty() {
+        return isDirty;
+    }
+
+    /* Returns an instance of our snapshot */
+    protected synchronized WindowMemento getSnapshot() {
+        return snapshot;
+    }
+
+    /* APP METHODS */
+
+    public synchronized void setWidth(int width) {
         if (this.width != width) {
             this.width = width;
-            this.isDirty = true;
+            markDirty();
         }
     }
 
-    protected synchronized void setHeight(int height) {
+    public synchronized void setHeight(int height) {
         if (this.height != height) {
             this.height = height;
-            this.isDirty = true;
+            markDirty();
         }
     }
 
-    protected synchronized void setXCoord(int xCoord) {
+    public synchronized void setXCoord(int xCoord) {
         if (this.xCoord != xCoord) {
             this.xCoord = xCoord;
-            this.isDirty = true;
+            markDirty();
         }
     }
 
-    protected synchronized void setYCoord(int yCoord) {
+    public synchronized void setYCoord(int yCoord) {
         if (this.yCoord != yCoord) {
             this.yCoord = yCoord;
-            this.isDirty = true;
+            markDirty();
         }
     }
 
-    protected synchronized void setBackgroundColor(Color backgroundColor) {
+    public synchronized void setBackgroundColor(Color backgroundColor) {
         if (!this.backgroundColor.equals(backgroundColor)) {
             this.backgroundColor = backgroundColor;
-            this.isDirty = true;
+            markDirty();
         }
     }
 
-    protected synchronized void setBackgroundColor(int red, int green, int blue) {
-        if (!this.backgroundColor.equals(new Color(red, green, blue))) {    
+    public synchronized void setBackgroundColor(int red, int green, int blue) {
+        if (this.backgroundColor.getRGB() != new Color(red, green, blue).getRGB()) {    
             this.backgroundColor = new Color(red, green, blue);
-            this.isDirty = true;
+            markDirty();
         }
     }
 
-    protected synchronized void setVisible(boolean isVisible) {
+    public synchronized void setVisible(boolean isVisible) {
         if (this.isVisible != isVisible) {    
             this.isVisible = isVisible;
-            this.isDirty = true;
+            markDirty();
         }
     }
 
-    protected synchronized void setFocusable(boolean isFocusable) {
+    public synchronized void setMinimized(boolean isMinimized) {
+        if (this.isMinimized != isMinimized) {
+            this.isMinimized = isMinimized;
+            markDirty();
+        }
+    }
+
+    public synchronized void setFocusable(boolean isFocusable) {
         if (this.isFocusable != isFocusable) {    
             this.isFocusable = isFocusable;
-            this.isDirty = true;
+            markDirty();
+        }
+    }
+
+    public synchronized void setHasFocus(boolean hasFocus) {
+        if (this.hasFocus != hasFocus) {
+            this.hasFocus = hasFocus;
+            markDirty();
         }
     }
 
@@ -130,10 +178,6 @@ public class Window {
         return yCoord;
     }
 
-    public synchronized String getTitle() {
-        return title;
-    }
-
     public synchronized Color getBackgroundColor() {
         return backgroundColor;
     }
@@ -142,8 +186,20 @@ public class Window {
         return isVisible;
     }
 
+    public synchronized boolean isMinimized() {
+        return isMinimized;
+    }
+
     public synchronized boolean isFocusable() {
         return isFocusable;
+    }
+
+    public synchronized boolean hasFocus() {
+        return hasFocus;
+    }
+
+    public String getTitle() {
+        return title;
     }
 
     public String getAppImageFilePath() {
@@ -164,32 +220,6 @@ public class Window {
 
     public boolean isOpaque() {
         return isOpaque;
-    }
-
-    /* Should only be read/written/modified by WindowRenderer */
-    protected synchronized void dirty() {
-        this.isDirty = false;
-    }
-
-    /* Should only be read/written/modified by WindowRenderer */
-    protected synchronized void clean() {
-        this.isDirty = false;
-    }
-
-    /* Should only be read/written/modified by WindowRenderer */
-    protected synchronized boolean isDirty() {
-        return isDirty;
-    }
-
-    protected synchronized void updateSnapshot() {
-        if (isDirty) {
-            renderWindow.copyFrom(liveWindow);
-            isDirty = false;
-        }
-    }
-
-    protected synchronized WindowState getSnapshot() {
-        return renderWindow;
     }
 
 }
