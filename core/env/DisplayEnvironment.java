559677cb9b68869be9fe54821f3da9308eec8c03
SantiagoF21 <santiagofdz18@gmail.com>
2026-02-18 16:05:32 -0500

    Stopped using Toolkit and now using GraphicsEnvironment to support multi-display environments.

--- core/env/DisplayEnvironment.java
+++ core/env/DisplayEnvironment.java
@@ -1,21 +1,22 @@
 package core.env;
 
-import java.awt.Dimension;
 import java.awt.GraphicsDevice;
 import java.awt.GraphicsEnvironment;
-import java.awt.Toolkit;
+import java.awt.Rectangle;
 
 public final class DisplayEnvironment {
 
     private DisplayEnvironment() {}
 
-    private static final GraphicsEnvironment DISPLAY_ENVIRONMENT = GraphicsEnvironment.getLocalGraphicsEnvironment();
+    private static final GraphicsEnvironment GRAPHICS_ENVIRONMENT = GraphicsEnvironment.getLocalGraphicsEnvironment();
 
-    public static final GraphicsDevice[] CURRENT_DISPLAY_DEVICES = DISPLAY_ENVIRONMENT.getScreenDevices();
+    public static final GraphicsDevice[] ALL_DEVICES = GRAPHICS_ENVIRONMENT.getScreenDevices();
+
+    public static final GraphicsDevice PRIMARY_DEVICE = GRAPHICS_ENVIRONMENT.getDefaultScreenDevice();
+
+    public static final Rectangle PRIMARY_DEVICE_BOUNDS = PRIMARY_DEVICE.getDefaultConfiguration().getBounds();
     
-    public static final Dimension SCREEN_SIZE = Toolkit.getDefaultToolkit().getScreenSize();
-    
-    public static final int SCREEN_WIDTH = (int)SCREEN_SIZE.getWidth();
-    public static final int SCREEN_HEIGHT = (int)SCREEN_SIZE.getHeight();
+    public static final int SCREEN_WIDTH = PRIMARY_DEVICE_BOUNDS.width;
+    public static final int SCREEN_HEIGHT = PRIMARY_DEVICE_BOUNDS.height;
 
 }
